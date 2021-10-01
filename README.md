33142cef3f6f787724caab63d01cc93db450b770
Aryaman <aryamansirohi@gmail.com>
2021-10-01 13:37:34 +0530

    Different thumbnails for different pictures

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Responsive site that resembles given pictures
 
-issues:
+Issues:
 - Have not used chisel
-- Images are not hovering
-- Just realized overlay for each thumbnail will be different
+- Should replace multiple functions with if-else or an analogue
+- Could also have used JS to inject text instead of making 4 separate divs with diff ids
 
 [Link to website](https://aryamansir02.github.io/sdswebp1/)
--- test.js
+++ test.js
@@ -1,6 +1,26 @@
-function overlayOn() {
-    document.getElementById("overlay").style.display = "block";
+function overlayOn1() {
+    document.getElementById("overlay1").style.display = "block";
 }
-function overlayOff() {
-    document.getElementById("overlay").style.display = "none";
+function overlayOff1() {
+    document.getElementById("overlay1").style.display = "none";
 }
+function overlayOn2() {
+    document.getElementById("overlay2").style.display = "block";
+}
+function overlayOff2() {
+    document.getElementById("overlay2").style.display = "none";
+}
+function overlayOn3() {
+    document.getElementById("overlay3").style.display = "block";
+}
+function overlayOff3() {
+    document.getElementById("overlay3").style.display = "none";
+}
+function overlayOn4() {
+    document.getElementById("overlay4").style.display = "block";
+}
+function overlayOff4() {
+    document.getElementById("overlay4").style.display = "none";
+}
+// should replace multiple functions with if-else or an analogue
+// could also have used js to inject text instead of making 4 separate divs with diff ids
